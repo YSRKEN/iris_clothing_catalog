4aba0a64b3b379c9c08b0b5fb3c8a26325a3776b
YSRKEN <ysr.ken@gmail.com>
2020-11-13 22:34:20 +0900

    [add] BootstrapおよびReact-Bootstrapを導入した

--- react/iris-clothing-catalog/src/App.tsx
+++ react/iris-clothing-catalog/src/App.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+
+const APPLICATION_TITLE = 'めーおーの聖装カタログ';
+
+const Title: React.FC = () => (<>
+  <h1 className="d-none d-sm-inline">{APPLICATION_TITLE}</h1>
+  <h3 className="d-inline d-sm-none">{APPLICATION_TITLE}</h3>
+</>);
 
 const App: React.FC = () => {
   return (
-    <span>test</span>
+    <Container>
+      <Row className="my-3">
+        <Col className="text-center">
+          <Title />
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
