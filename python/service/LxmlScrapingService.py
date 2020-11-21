93b574da11924089b551ffdb66be0cf0ccb2f777
YSRKEN <ysr.ken@gmail.com>
2020-11-22 03:19:15 +0900

    [add] データをキャッシュするシステムを追加した

--- python/service/LxmlScrapingService.py
+++ python/service/LxmlScrapingService.py
@@ -6,16 +6,27 @@ import requests
 from model import DomObject
 from model.LxmlDomObject import LxmlDomObject
 from service.ScrapingService import ScrapingService
+from service.i_database_service import IDataBaseService
 
 
 class LxmlScrapingService(ScrapingService):
     """スクレイピング用のラッパークラス"""
 
-    def get_page(self, url: str, encoding='') -> DomObject:
-        response = requests.get(url)
-        if encoding != '':
-            response.encoding = encoding
-        text = response.text.encode(response.encoding, 'ignore').decode(response.encoding, 'ignore')\
-            .encode(response.encoding, 'ignore')
-        time.sleep(1)
-        return LxmlDomObject(lxml.html.fromstring(text))
+    def __init__(self, database: IDataBaseService):
+        self.database = database
+        self.database.query('CREATE TABLE IF NOT EXISTS page_cache (url TEXT PRIMARY KEY, text TEXT)')
+
+    def get_page(self, url: str, encoding='', cache=True) -> DomObject:
+        cache_data = self.database.select('SELECT text from page_cache WHERE url=?', (url,))
+        if len(cache_data) == 0 or not cache:
+            response = requests.get(url)
+            if encoding != '':
+                response.encoding = encoding
+            text = response.text.encode(response.encoding, 'ignore').decode(response.encoding, 'ignore') \
+                .encode(response.encoding, 'ignore')
+            time.sleep(1)
+            print(f'caching... [{url}]')
+            self.database.query('INSERT INTO page_cache (url, text) VALUES (?, ?)', (url, text))
+            return LxmlDomObject(lxml.html.fromstring(text))
+        else:
+            return LxmlDomObject(lxml.html.fromstring(cache_data[0]['text']))
