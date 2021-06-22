a96c024080f89db5c2529c81690d02e90c3db148
YSRKEN <ysr.ken@gmail.com>
2021-06-22 15:09:56 +0900

    [wip] スクレイピングするコードをリファクタリング開始

--- python/main.py
+++ python/main.py
@@ -1,72 +1,127 @@
+from pprint import pprint
 from typing import List, Dict
 
+from model.DomObject import DomObject
 from model.IrisClothing import IrisClothing, Skill
 from service.LxmlScrapingService import LxmlScrapingService
+from service.ScrapingService import ScrapingService
 from service.i_database_service import IDataBaseService
 from service.sqlite_database_service import SqliteDataBaseService
 
-if __name__ == '__main__':
-    # スクレイピングの準備
-    database: IDataBaseService = SqliteDataBaseService('database.db')
-    scraping = LxmlScrapingService(database)
+DRESS_LIST_URL = 'https://xn--l8je7d7jnef7m6d8j6d.xn--wiki-4i9hs14f.com/index.php?%E6%80%A7%E8%83%BD%E4%B8%80%E8%A6%A7'
 
-    # 一覧ページを読み取って、一覧を作成する
-    dom = scraping.get_page('https://xn--l8je7d7jnef7m6d8j6d.xn--wiki-4i9hs14f.com/index.php?%E6%80%A7%E8%83%BD%E4%B8'
-                            '%80%E8%A6%A7', 'utf-8', cache=False)
-    data_list: List[IrisClothing] = []
+
+def is_dress_list_table_tag(table_tag: DomObject) -> bool:
+    thead_tag = table_tag.find('thead')
+    if thead_tag is None:
+        return False
+    thead_text = thead_tag.full_text
+    if 'HP' not in thead_text:
+        return False
+
+    return True
+
+
+def get_raw_dress_list(table_tag: DomObject) -> List[Dict[str, str]]:
+    th_text_list = [x.text for x in table_tag.find('thead').find_all('th')]
+    dress_name_index = th_text_list.index('名前')
+    dress_type_index = th_text_list.index('属性')
+    dress_hp_index = th_text_list.index('HP')
+    dress_attack_index = th_text_list.index('攻撃')
+    dress_defence_index = th_text_list.index('防御')
+    dress_magic_index = th_text_list.index('魔力')
+    dress_speed_index = th_text_list.index('敏捷')
+    dress_lucky_index = th_text_list.index('幸運')
+    dress_evade_index = th_text_list.index('回避率')
+    dress_counter_index = th_text_list.index('反撃率')
+    dress_death_index = th_text_list.index('即死率')
+
+    table_text = table_tag.full_text
+    if '高潔なる煌めきの銀閃' in table_text:
+        dress_reality = 'SSR'
+    elif '白銀の疾風' in table_text:
+        dress_reality = 'SR'
+    elif '忠信の私服' in table_text:
+        dress_reality = 'R'
+    elif '騎士の私服' in table_text:
+        dress_reality = 'N'
+    else:
+        dress_reality = ''
+
+    raw_dress_list: List[Dict[str, str]] = []
+    for tr_tag in table_tag.find_all('tbody > tr'):
+        # 基礎データを取得
+        td_text_list = [x.full_text for x in tr_tag.find_all('td')]
+        dress_name = td_text_list[dress_name_index]
+        dress_type = td_text_list[dress_type_index]
+        dress_hp = td_text_list[dress_hp_index]
+        dress_attack = td_text_list[dress_attack_index]
+        dress_defence = td_text_list[dress_defence_index]
+        dress_magic = td_text_list[dress_magic_index]
+        dress_speed = td_text_list[dress_speed_index]
+        dress_lucky = td_text_list[dress_lucky_index]
+        dress_evade = td_text_list[dress_evade_index]
+        dress_counter = td_text_list[dress_counter_index]
+        dress_death = td_text_list[dress_death_index]
+        dress_link = tr_tag.find_all('td')[dress_name_index].find('a').attrs['href']
+        temp = dress_name.split('】')
+        dress_nickname = temp[0].replace('【', '')
+        dress_iris_name = temp[1]
+        raw_dress_list.append({
+            'reality': dress_reality,
+            'nickname': dress_nickname,
+            'iris_name': dress_iris_name,
+            'type': dress_type,
+            'hp': dress_hp,
+            'attack': dress_attack,
+            'defence': dress_defence,
+            'magic': dress_magic,
+            'speed': dress_speed,
+            'lucky': dress_lucky,
+            'evade': dress_evade,
+            'counter': dress_counter,
+            'death': dress_death,
+            'link': dress_link,
+        })
+    return raw_dress_list
+
+
+def get_dress_list(scraping: ScrapingService) -> List[IrisClothing]:
+    dress_list: List[IrisClothing] = []
+
+    # 一覧のデータを取得する
+    dom = scraping.get_page(DRESS_LIST_URL, 'utf-8', cache=False)
+    # 各テーブルを確認し、聖装一覧なら更に読み込みを続ける
     for table_tag in dom.find_all('table'):
-        # ヘッダーから、聖装一覧かどうかの検討をつける
-        thead_tag = table_tag.find('thead')
-        if thead_tag is None:
+        # 聖装一覧かどうかを判定する
+        if not is_dress_list_table_tag(table_tag):
             continue
-        thead_text = thead_tag.full_text
-        if 'HP' not in thead_text:
-            continue
-        th_text_list = [x.text for x in thead_tag.find_all('th')]
-        name_index = th_text_list.index('名前')
-        type_index = th_text_list.index('属性')
-        hp_index = th_text_list.index('HP')
-        attack_index = th_text_list.index('攻撃')
-        defence_index = th_text_list.index('防御')
-        magic_index = th_text_list.index('魔力')
-        speed_index = th_text_list.index('敏捷')
-        lucky_index = th_text_list.index('幸運')
-        evade_index = th_text_list.index('回避率')
-        counter_index = th_text_list.index('反撃率')
-        death_index = th_text_list.index('即死率')
 
-        # 基礎データを取得する
-        table_text = table_tag.full_text
-        if '高潔なる煌めきの銀閃' in table_text:
-            reality = 'SSR'
-        elif '白銀の疾風' in table_text:
-            reality = 'SR'
-        elif '忠信の私服' in table_text:
-            reality = 'R'
-        elif '騎士の私服' in table_text:
-            reality = 'N'
-        else:
-            reality = ''
+        # 聖装一覧なので、各列の情報をざっくり読み取る
+        raw_dress_list = get_raw_dress_list(table_tag)
+        for raw_dress in raw_dress_list:
+            print(raw_dress)
+    return dress_list
 
-        for tr_tag in table_tag.find_all('tbody > tr'):
-            # 基礎データを取得
-            td_text_list = [x.full_text for x in tr_tag.find_all('td')]
-            cloth_name = td_text_list[name_index]
-            cloth_type = td_text_list[type_index]
-            cloth_hp = td_text_list[hp_index]
-            cloth_attack = td_text_list[attack_index]
-            cloth_defence = td_text_list[defence_index]
-            cloth_magic = td_text_list[magic_index]
-            cloth_speed = td_text_list[speed_index]
-            cloth_lucky = td_text_list[lucky_index]
-            cloth_evade = td_text_list[evade_index]
-            cloth_counter = td_text_list[counter_index]
-            cloth_death = td_text_list[death_index]
-            temp = cloth_name.split('】')
-            nickname = temp[0].replace('【', '')
-            iris_name = temp[1]
-            print(f'{reality} 【{nickname}】{iris_name.replace(",", "")}')
 
+def main(db_path: str, save_path: str):
+    # スクレイピングの準備
+    database: IDataBaseService = SqliteDataBaseService(db_path)
+    scraping: ScrapingService = LxmlScrapingService(database)
+
+    # スクレイピング処理
+    dress_list = get_dress_list(scraping)
+
+    # 結果を保存する
+    # with open(save_path, 'w', encoding='UTF-8') as f:
+    #     f.write(IrisClothing.schema().dumps(dress_list, many=True, ensure_ascii=False, indent=2, sort_keys=True))
+
+
+if __name__ == '__main__':
+    main('database.db', 'list.json')
+
+
+"""
             # 萌技・スキル・アビリティを取得
             if '?' not in iris_name:
                 cloth_link = tr_tag.find_all('td')[name_index].find('a').attrs['href']
@@ -130,6 +185,4 @@ if __name__ == '__main__':
                 data_list.append(cloth_data)
                 print(cloth_data)
 
-    # 保存
-    with open('list.json', 'w', encoding='UTF-8') as f:
-        f.write(IrisClothing.schema().dumps(data_list, many=True, ensure_ascii=False, indent=2))
+"""
--- python/service/ScrapingService.py
+++ python/service/ScrapingService.py
@@ -7,13 +7,14 @@ class ScrapingService(metaclass=ABCMeta):
     """スクレイピング用のラッパークラス"""
 
     @abstractmethod
-    def get_page(self, url: str, encoding='') -> DomObject:
+    def get_page(self, url: str, encoding='', cache=False) -> DomObject:
         """WebページのDOMオブジェクトを取得する
 
         Parameters
         ----------
         url URL
         encoding 文字エンコーディング(空文字列なら自動判定)
+        cache キャッシュ機構をONにする場合はTrue
 
         Returns
         -------
