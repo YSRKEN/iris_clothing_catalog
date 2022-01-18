30cc82668d9cfa35bd55003217ffb3cce20c63df
YSR <ysr.ken@gmail.com>
2022-01-18 12:37:42 +0900

    [update] データを更新

--- react/iris-clothing-catalog/src/App.tsx
+++ react/iris-clothing-catalog/src/App.tsx
@@ -516,7 +516,7 @@ const MainForm: React.FC = () => {
         </Row>
         <Row>
           <Col className="text-center">
-            <span className="d-inline-block mr-3">最終更新：2021/12/30</span>
+            <span className="d-inline-block mr-3">最終更新：2022/01/18</span>
             <span className="d-inline-block mr-3"><a href="https://twitter.com/YSRKEN/status/1340182344439717891" rel="noreferrer" target="_blank">使い方</a></span>
             <span className="d-inline-block mr-3"><a href="https://github.com/YSRKEN/iris_clothing_catalog" rel="noreferrer" target="_blank">GitHub</a> </span>
             <span><a href="https://twitter.com/YSRKEN" rel="noreferrer" target="_blank">作者のTwitter</a></span>
